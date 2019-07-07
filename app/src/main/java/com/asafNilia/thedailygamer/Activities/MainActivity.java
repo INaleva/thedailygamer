86fc8b1d387326a4df59729b5c2a561b28c798bc
ilia naleva <ilianaleva@gmail.com>
2019-07-07 12:40:11 +0300

    concept 0.1

--- app/src/main/java/com/asafNilia/thedailygamer/Activities/MainActivity.java
+++ app/src/main/java/com/asafNilia/thedailygamer/Activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.asafNilia.thedailygamer;
+package com.asafNilia.thedailygamer.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.asafNilia.thedailygamer.R;
 
 public class MainActivity extends AppCompatActivity {
 
