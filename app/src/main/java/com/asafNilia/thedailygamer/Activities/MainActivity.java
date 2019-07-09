95263bc6f350453d209984adac911506e5c0149e
ilia naleva <ilianaleva@gmail.com>
2019-07-09 13:10:40 +0300

    buttons now switch fragments, search works by hitting enter

--- app/src/main/java/com/asafNilia/thedailygamer/Activities/MainActivity.java
+++ app/src/main/java/com/asafNilia/thedailygamer/Activities/MainActivity.java
@@ -1,5 +1,6 @@
 package com.asafNilia.thedailygamer.Activities;
 
+import android.animation.ObjectAnimator;
 import android.net.Uri;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
@@ -9,6 +10,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.asafNilia.thedailygamer.Fragments.gameItem;
@@ -35,6 +38,9 @@ public class MainActivity extends AppCompatActivity implements
 
     String toSearch;
     EditText textSearchField;
+    ImageView homeButton;
+    ImageView menuButton;
+    ImageView searchButton;
     boolean searchTextIsVisible = false;
     FragmentTransaction fragmentTransaction;
 
@@ -44,6 +50,9 @@ public class MainActivity extends AppCompatActivity implements
         setContentView(R.layout.activity_main);
 
         textSearchField = findViewById(R.id.textSearch);
+        homeButton = findViewById(R.id.homeButton);
+        menuButton = findViewById(R.id.buttonOpenMenu);
+        searchButton = findViewById(R.id.buttonSearchGame);
 
         //on click listener for key 'ENTER', as for now we only print the text that was written.
         textSearchField.setOnKeyListener(new View.OnKeyListener() {
@@ -58,6 +67,36 @@ public class MainActivity extends AppCompatActivity implements
                 return false;
             }
         });        //apply newGames fragment to the fragment on start.
+
+        homeButton.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Toast.makeText(MainActivity.this, "Home screen, lets to see all the cool games!", Toast.LENGTH_SHORT).show();
+                ObjectAnimator.ofFloat(v, "rotation", 0, 360).start();
+
+                return true;
+            }
+        });
+
+        menuButton.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Toast.makeText(MainActivity.this, "Main menu, helps you to see only the games you like!", Toast.LENGTH_SHORT).show();
+                ObjectAnimator.ofFloat(v, "rotation", 0, 360).start();
+
+                return true;
+            }
+        });
+
+        searchButton.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Toast.makeText(MainActivity.this, "Search for that game you really liked!", Toast.LENGTH_SHORT).show();
+                ObjectAnimator.ofFloat(v, "rotation", 0, 360).start();
+
+                return true;
+            }
+        });
         fragmentTransaction = getSupportFragmentManager().beginTransaction();
         fragmentTransaction.replace(R.id.fragment_frame_layout, new newGames());
         fragmentTransaction.addToBackStack(null);
