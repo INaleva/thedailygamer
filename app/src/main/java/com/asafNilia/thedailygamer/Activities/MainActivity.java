0576abba78dcf236fd8910923911c055d8e92a7f
ilia naleva <ilianaleva@gmail.com>
2019-07-09 12:53:32 +0300

    buttons now switch fragments, search works by hitting enter

--- app/src/main/java/com/asafNilia/thedailygamer/Activities/MainActivity.java
+++ app/src/main/java/com/asafNilia/thedailygamer/Activities/MainActivity.java
@@ -1,9 +1,15 @@
 package com.asafNilia.thedailygamer.Activities;
 
 import android.net.Uri;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.asafNilia.thedailygamer.Fragments.gameItem;
 import com.asafNilia.thedailygamer.Fragments.menu;
@@ -27,13 +33,36 @@ public class MainActivity extends AppCompatActivity implements
     private ArrayList<String> mCreator = new ArrayList<>();
     private ArrayList<String> mGenre = new ArrayList<>();
 
+    String toSearch;
+    EditText textSearchField;
+    boolean searchTextIsVisible = false;
+    FragmentTransaction fragmentTransaction;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //  initImages();
-        //initRecylerView();
+        textSearchField = findViewById(R.id.textSearch);
+
+        //on click listener for key 'ENTER', as for now we only print the text that was written.
+        textSearchField.setOnKeyListener(new View.OnKeyListener() {
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                // If the event is a key-down event on the "enter" button
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) &&
+                        (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    // Perform action on key press
+                    Toast.makeText(MainActivity.this,"Searching for: " + textSearchField.getText(), Toast.LENGTH_SHORT).show();
+                    return true;
+                }
+                return false;
+            }
+        });        //apply newGames fragment to the fragment on start.
+        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_frame_layout, new newGames());
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+
     }
 
 
@@ -72,4 +101,50 @@ public class MainActivity extends AppCompatActivity implements
     public void onUpcomingFragmentInteraction(Uri uri) {
 
     }
+
+    public void searchClicked(View view) {
+
+        if(searchTextIsVisible)
+        {
+            textSearchField.setVisibility(View.GONE);
+            searchTextIsVisible = false;
+            changeFragment(new newGames());
+        }
+        else
+        {
+            textSearchField.setVisibility(View.VISIBLE);
+            searchTextIsVisible = true;
+        }
+
+
+    }
+
+    public void openMenu(View view)
+    {
+        cancelSearch();
+        changeFragment(new menu());
+
+    }
+
+    public void openHome(View view)
+    {
+        cancelSearch();
+        changeFragment(new newGames());
+    }
+
+    void cancelSearch() //removes the search bar
+    {
+        textSearchField.setVisibility(View.GONE);
+        searchTextIsVisible = false;
+    }
+
+    void changeFragment(Fragment fragment)
+    {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_frame_layout,fragment);
+        transaction.commit();
+
+    }
+
+
 }
